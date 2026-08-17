34a2c799528acf1c03a57290c942aab1681556e5
ItzIvy05 <itzskyfo@gmail.com>
2026-08-17 09:22:37 -0400

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -192,6 +192,7 @@ namespace {
                         }
                         ReconcileSystemEnabledState();
                         TearsWidget::TickAutoHide();
+                        SurvivalWidgets::Notify();
                         SurvivalWidgets::TickAutoHide();
                     });
                 }
